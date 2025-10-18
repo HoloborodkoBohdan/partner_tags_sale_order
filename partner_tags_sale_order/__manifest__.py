7daa365e2b57f7bfb3752b03e4ebc75b0ddc2dea
Holoborodko Bohdan <holoborodko.bohdan@gmail.com>
2025-10-18 16:21:20 +0200

    fix: update version in manifest

--- partner_tags_sale_order/__manifest__.py
+++ partner_tags_sale_order/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020 Holoborodko Bohdan
+# Copyright 2025 Holoborodko Bohdan
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Partner Tags in Sale Order",
     "summary": "Show partner tags in sale order",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Tools",
     "website": "https://holoborodko.com.ua/?ref=partner_tags_sale_order",
     "author": "Bohdan Holoborodko",
